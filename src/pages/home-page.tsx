fc33a0d73a6f164282070201609a7e429099239a
Lredigonda <leoredigonda@hotmail.com>
2022-10-03 21:16:25 -0300

    Add title to home panel and fixing some styles

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,26 @@
+import "./home-page.scss";
+
 function Homepage() {
   return (
-    <div>
-      <h1>Beta React Portfolio</h1>
+    <div className="homepage">
+      <div className="homepage__container">
+        <div className="homepage__title-container">
+          <h1 className="homepage__title-container__title">👋Bienvenido!</h1>
+          <h2 className="homepage__title-container__subtitle">
+            Soy <b>Leonel Redigonda</b>
+          </h2>
+          <h3 className="homepage__title-container__info">
+            Front End <b>Developer</b>
+          </h3>
+        </div>
+        <div className="homepage__image-container">
+          <img
+            src="../assets/Coding.svg"
+            alt="home-image"
+            className="homepage__image"
+          />
+        </div>
+      </div>
     </div>
   );
 }
