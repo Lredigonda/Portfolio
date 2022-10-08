3c52e79a6073899eb738213491f30433f49fc978
Lredigonda <leoredigonda@hotmail.com>
2022-10-08 14:34:50 -0300

    Add info in home panel

--- src/app-components/about-me.tsx
+++ src/app-components/about-me.tsx
@@ -11,22 +11,21 @@ function AboutMe() {
           Un poco <b>sobre mi</b>
         </h2>
         <p className="about-me__info-1">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-          commodi, placeat assumenda modi voluptates praesentium fugiat aut
-          voluptatem veniam vero debitis, pariatur illo. Quaerat accusamus, ab
-          laboriosam in porro itaque!
+          Me interesa mucho el desarrollo de aplicaciones web y aquirir
+          conocimiento en nuevas tecnologias.
         </p>
         <p className="about-me__info-2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-          commodi, placeat assumenda modi voluptates praesentium fugiat aut
-          voluptatem veniam vero debitis, pariatur illo. Quaerat accusamus, ab
-          laboriosam in porro itaque!
+          Soy desarrollador <b>FrontEnd</b> con conocimientos basicos de
+          BackEnd.
+          <br />
+          Tengo conocimientos en <b>maquetacion</b> y <b>Javascript</b>, junto
+          con algunas de sus <b>librerias</b>.
         </p>
         <p className="about-me__info-3">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-          commodi, placeat assumenda modi voluptates praesentium fugiat aut
-          voluptatem veniam vero debitis, pariatur illo. Quaerat accusamus, ab
-          laboriosam in porro itaque!
+          Soy <b>estudiante</b> de la carrera universitaria "Desarrollo de
+          Software".
+          <br />
+          Me gusta estar en un constante <b>aprendizaje</b>.
         </p>
       </div>
     </section>
