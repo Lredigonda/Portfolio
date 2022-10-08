68b59f730ae0d0ce653fe8732729f644368f9790
Leonel Redigonda <67444819+Lredigonda@users.noreply.github.com>
2022-10-08 08:37:30 -0300

    Add footer component (#4)
    * Create and set up footer component
    
    * Add logo images for footer buttons
    
    * Fixing footer styles
    
    * Add darkmode button component
    
    * Fixing footer styles
    
    * Add translate button component and fix darkmode button

--- src/app-components/about-me.tsx
+++ src/app-components/about-me.tsx
@@ -3,6 +3,9 @@ import "./about-me.scss";
 function AboutMe() {
   return (
     <section className="about-me">
+      <div className="about-me__image-container">
+        <div className="about-me__image"></div>
+      </div>
       <div className="about-me__info-container">
         <h2 className="about-me__title">
           Un poco <b>sobre mi</b>
@@ -25,9 +28,6 @@ function AboutMe() {
           voluptatem veniam vero debitis, pariatur illo. Quaerat accusamus, ab
           laboriosam in porro itaque!
         </p>
-      </div>
-      <div className="about-me__image-container">
-        <div className="about-me__image"></div>
       </div>
     </section>
   );
---
+++ src/core-components/darkmode-button.tsx
@@ -0,0 +1,28 @@
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+import "../core-components/darkmode-button.scss";
+
+export interface darkmodeButtonProps {
+  className?: string;
+}
+
+function DarkModeButton(className: darkmodeButtonProps) {
+  const [theme, setTheme] = useState(true);
+  const toggleTheme = () => {
+    setTheme(!theme);
+  };
+  return (
+    <button
+      className={`darkmode-button ${className ? className : ``}`}
+      onClick={toggleTheme}
+    >
+      <FontAwesomeIcon
+        icon={theme === true ? faMoon : faSun}
+        className="darkmode-button__icon"
+      />
+    </button>
+  );
+}
+export default DarkModeButton;
